30cb9e3f1a00e409175bbd77a73b57eab39a23b9
Stacy <jlhclientsolutions@gmail.com>
2024-08-15 16:38:01 -0400

    added appropriate code to existing files

---
+++ models/index.js
@@ -0,0 +1,40 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  OnUpdate: 'CASCADE',
+  onDelete: 'SET NULL',
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'tag_id',
+  otherKey: 'product_id',
+  through: ProductTag,
+  as: 'Product',
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+   foreignKey: 'product_id',
+   otherKey: 'tag_id',
+   through: ProductTag,
+   as: 'Tag',
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
