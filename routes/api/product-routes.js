979462e6af71c309a84444139c7bbfc9933ca8d1
Stacy <jlhclientsolutions@gmail.com>
2024-08-16 09:07:22 -0400

    made a correction to product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -39,15 +39,13 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', async (req, res) => {
-  /* req.body should look like this...*/
-  const newProduct = JSON.Stringify({
-        product_name: "Basketball",
-        price: 200.00,
-        stock: 3,
-        tagIds: [1, 2, 3, 4],
-        }) = req.body
-      await Product.create(req.body)
-      res.json(newProduct)
+  /* req.body should look like this...
+    {
+    product_name: "Basketball",
+    price: 200.00,
+    stock: 3,
+    tagIds: [1, 2, 3, 4],
+    } */
     
 
   Product.create(req.body)
